091dee7eea246ace44c47d11eb1540deee5252c0
vitos2412 <73468190+vitos2412@users.noreply.github.com>
2020-10-26 13:29:39 +0300

    Add files via upload

---
+++ Dogi.java
@@ -0,0 +1,11 @@
+
+public class Dogi extends Dog {
+    String name = "Dogi";
+
+    @Override
+    public String toStringAbstract() {
+        return "Dogi{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
